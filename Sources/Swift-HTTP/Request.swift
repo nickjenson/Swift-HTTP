67a3e505d6562ce14f33f48fbdf20f695ef94756
Nick Jenson <nickjenson@gmail.com>
2022-02-06 16:09:23 -0800

    Add definitions for HTTPBody, default HTTPBody, EmptyBody, DataBody, JSONBody and FormBody

--- Sources/Swift-HTTP/Request.swift
+++ Sources/Swift-HTTP/Request.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Infinite number of single-word methods, using enum would limit us
 public struct HTTPMethod: Hashable {
     public static let get = HTTPMethod(rawValue: "GET")
     public static let post = HTTPMethod(rawValue: "POST")
@@ -31,26 +32,28 @@ public struct HTTPStatus: Hashable {
     public let rawValue: Int
 }
 
+// Represents 1. request line (method, path) 2. Headers and 3. Body
 public struct HTTPRequest {
-    private var urlComponents = URLComponents()
-    public var method: HTTPMethod = .get // defined above
+    private var urlComponents = URLComponents() // does heavy lifting of fully-formed URL handling
+    public var method: HTTPMethod = .get
     public var headers: [String: String] = [:]
-    public var body: Data?
+    public var body: HTTPBody = EmptyBody() // was optional "HTTPBody?", updated to default to EmptyBody()
     
     public init() {
         urlComponents.scheme = "https"
     }
 }
 
+// Selectively re-exposing portions of [private] urlComponents
 public extension HTTPRequest {
-    public var scheme: String { urlComponents.scheme ?? "https" }
+    var scheme: String { urlComponents.scheme ?? "https" }
     
-    public var host: String? {
+    var host: String? {
         get { urlComponents.host }
         set { urlComponents.host = newValue }
     }
     
-    public var path: String {
+    var path: String {
         get { urlComponents.path }
         set { urlComponents.path = newValue }
     }
@@ -58,7 +61,7 @@ public extension HTTPRequest {
 
 public struct HTTPResponse {
     public let request: HTTPRequest // helpful to have request in response
-    private let response: HTTPURLResponse
+    private let response: HTTPURLResponse // represents all of response _except_ body
     public let body: Data?
     
     public var status: HTTPStatus {
@@ -72,6 +75,7 @@ public struct HTTPResponse {
     public var headers: [AnyHashable: Any] { response.allHeaderFields }
 }
 
+// We won't _always_ get a response, need to encapsulate those scenarios with HTTPResult
 public typealias HTTPResult = Result<HTTPResponse, HTTPError>
 
 public struct HTTPError: Error {
@@ -97,6 +101,7 @@ public struct HTTPError: Error {
     }
 }
 
+// Having an HTTPResult means we have an HTTPRequest
 extension HTTPResult {
     
     public var request: HTTPRequest {
@@ -114,3 +119,92 @@ extension HTTPResult {
     }
     
 }
+
+// Protocol instead of concrete type to not restrict data construction
+public protocol HTTPBody {
+    // helps us avoid attempting to retrieve encoded data if there isn't any
+    var isEmpty: Bool { get }
+    var additionalHeaders: [String: String] { get }
+    func encode() throws -> Data    // get data, optionally throw error
+}
+
+// Default implementation of HTTPBody
+extension HTTPBody {
+    public var isEmpty: Bool { return false }
+    public var additionalHeaders: [String: String] { return [:] }
+}
+
+// Defining EmptyBody
+public struct EmptyBody: HTTPBody {
+    public let isEmpty = true
+    
+    public init() {}
+    public func encode() throws -> Data { Data() }
+}
+
+public struct DataBody: HTTPBody {
+    private let data: Data
+    
+    public var isEmpty: Bool { data.isEmpty }
+    public var additionalHeaders: [String : String]
+    
+    public init(_ data: Data, additionalHeaders: [String: String] = [:]) {
+        self.data = data
+        self.additionalHeaders = additionalHeaders
+    }
+    
+    public func encode() throws -> Data { data }
+}
+
+public struct JSONBody: HTTPBody {
+    public let isEmpty: Bool = false
+    public var additionalHeaders = [
+        "Content-Type": "application/json; charset=utf-8"
+    ]
+    
+    private let encoder: () throws -> Data // changed to encoder
+    
+    public init<T: Encodable>(_ value: T, encoder: JSONEncoder = JSONEncoder()) {
+        self.encoder = { try encoder.encode(value) }
+    }
+    
+    public func encode() throws -> Data { return try encoder() }
+}
+
+// Form body should end up as URL-encoded key-value pairs: name=Aurther&age=42
+public struct FormBody: HTTPBody {
+    public var isEmpty: Bool { values.isEmpty }
+    public let additionalHeaders =  [
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+    ]
+    
+    private let values: [URLQueryItem]
+    
+    public init(_ values: [URLQueryItem]){
+        self.values = values
+    }
+    
+    public init (_ values: [String: String]) {
+        let queryItems = values.map { URLQueryItem(name: $0.key, value: $0.value) }
+        self.init(queryItems)
+    }
+    
+    public func encode() throws -> Data {
+        let pieces = values.map(self.urlEncode)
+        let bodyString = pieces.joined(separator: "&")
+        return Data(bodyString.utf8)
+    }
+
+    private func urlEncode(_ queryItem: URLQueryItem) -> String {
+        let name = urlEncode(queryItem.name)
+        let value = urlEncode(queryItem.value ?? "")
+        return "\(name)=\(value)"
+    }
+    
+    private func urlEncode(_ string: String) -> String {
+        let allowedCharacters = CharacterSet.alphanumerics
+        return string.addingPercentEncoding(withAllowedCharacters: allowedCharacters) ?? ""
+    }
+}
+
+
