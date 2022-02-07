9bb36f0280ce9811fe4cde58b5bc7abf504b9919
Nick Jenson <nickjenson@gmail.com>
2022-02-06 17:41:44 -0800

    Add base outline HTTPLoading

--- Sources/Swift-HTTP/Request.swift
+++ Sources/Swift-HTTP/Request.swift
@@ -43,3 +43,47 @@ public extension HTTPRequest {
         set { urlComponents.path = newValue }
     }
 }
+
+public protocol HTTPLoading {
+    func load(request: HTTPRequest, completion: @escaping (HTTPResult) -> Void)
+}
+
+extension URLSession: HTTPLoading {
+    public func load(request: HTTPRequest, completion: @escaping (HTTPResult) -> Void) {
+        guard let url = request.url else {
+            // can't construct proper URL out of request's URLComponents
+            completion(.failure(HTTPError(code: .invalidRequest, request: request, response: nil, underlyingError: nil)))
+            return
+        }
+        // construct request
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = request.method.rawValue
+        
+        // copy over any custom HTTP headers
+        for(header, value) in request.headers {
+            urlRequest.addValue(value, forHTTPHeaderField: header)
+        }
+        
+        if request.body.isEmpty == false {
+            // if body defines headers, add them
+            for(header, value) in request.body.additionalHeaders {
+                urlRequest.addValue(value, forHTTPHeaderField: header)
+            }
+            // attempt to retrieve the body data
+            do {
+                urlRequest.httpBody = try request.body.encode()
+            } catch {
+                // error creating body; stop and report back
+                completion(.failure(HTTPError(code: .invalidRequest, request: request, response: nil, underlyingError: nil)))
+                return
+            }
+        }
+        
+        let dataTask = session.dataTask(with: urlRequest) { (data, response, error) in
+            let result = HTTPResult(request: request, reponseData: data, reponse: response, error: error)
+            completion(result)
+        }
+        
+        dataTask.resume()
+    }
+}
